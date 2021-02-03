39c27301ed85967f308128a6da1e13b90c22041e
Ethan Ruiz <47010905+elruizz@users.noreply.github.com>
2021-02-02 20:58:46 -0700

    caesar.py
    caesar encryption with a shift value of 7 in my case. might make an option to make the shift a variable amount input by the user.

---
+++ caesar.py
@@ -0,0 +1,26 @@
+import string
+
+'''
+plain_text = "Hello World"
+shift = 7
+shift %= 26
+
+alphabet = string.ascii_lowercase
+shifted = alphabet[shift:] + alphabet{:shift]
+table str.maketrans(alphabet, shifted)
+
+encrypted = plain_text.translate(table)
+
+print(encrypted)
+'''
+def caesar(text, shift, alphabets):
+    def shift_alphabet(alphabet):
+        return alphabet[shift:] + alphabet[:shift]
+    shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+    final_alphabet = ''.join(alphabets)
+    final_shifted_alphabet = ''.join(shifted_alphabets)
+    table = str.maketrans(final_alphabet, final_shifted_alphabet)
+    return text.translate(table)
+    
+plain_text = "MYSUPERcomplicatedPASSWORD^%$"
+print(caesar(plain_text, 7, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
